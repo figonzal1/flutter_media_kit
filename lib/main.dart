3f2698c924afd7964c35ce738779c40927a606bd
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2024-04-19 10:05:50 -0400

    Ganatiempo + videos
    Signed-off-by: figonzal1 <felipe.gonzalezalarcon94@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-
 // Make sure to add following packages to pubspec.yaml:
 // * media_kit
 // * media_kit_video
 // * media_kit_libs_video
-import 'package:media_kit/media_kit.dart';                      // Provides [Player], [Media], [Playlist] etc.
-import 'package:media_kit_video/media_kit_video.dart';          // Provides [VideoController] & [Video] etc.        
+import 'package:media_kit/media_kit.dart'; // Provides [Player], [Media], [Playlist] etc.
+import 'package:media_kit_video/media_kit_video.dart'; // Provides [VideoController] & [Video] etc.
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,14 +18,20 @@ void main() {
 }
 
 class MyScreen extends StatefulWidget {
-  const MyScreen({Key? key}) : super(key: key);
+  const MyScreen({super.key});
   @override
   State<MyScreen> createState() => MyScreenState();
 }
 
 class MyScreenState extends State<MyScreen> {
   // Create a [Player] to control playback.
-  late final player = Player();
+  late final player = Player(
+      configuration: PlayerConfiguration(
+          title: "Titulo de ejemplo",
+          ready: () {
+            print("Init complete");
+          }));
+
   // Create a [VideoController] to handle video output from [Player].
   late final controller = VideoController(player);
 
@@ -34,7 +39,25 @@ class MyScreenState extends State<MyScreen> {
   void initState() {
     super.initState();
     // Play a [Media] or [Playlist].
-    player.open(Media('https://user-images.githubusercontent.com/28951144/229373695-22f88f13-d18f-4288-9bf1-c3e078d83722.mp4'));
+    final playable = Playlist(
+      [
+        Media(
+            'https://user-images.githubusercontent.com/28951144/229373695-22f88f13-d18f-4288-9bf1-c3e078d83722.mp4'),
+        Media(
+            'https://user-images.githubusercontent.com/28951144/229373709-603a7a89-2105-4e1b-a5a5-a6c3567c9a59.mp4'),
+        Media(
+            'https://user-images.githubusercontent.com/28951144/229373716-76da0a4e-225a-44e4-9ee7-3e9006dbc3e3.mp4'),
+        Media(
+            'https://user-images.githubusercontent.com/28951144/229373718-86ce5e1d-d195-45d5-baa6-ef94041d0b90.mp4'),
+        Media(
+            'https://user-images.githubusercontent.com/28951144/229373720-14d69157-1a56-4a78-a2f4-d7a134d7c3e9.mp4'),
+      ],
+    );
+
+    //await player.setPlaylistMode(PlaylistMode.loop);
+    //await player.setVolume(0);
+
+    player.open(playable);
   }
 
   @override
@@ -45,13 +68,20 @@ class MyScreenState extends State<MyScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.width * 9.0 / 16.0,
-        // Use [Video] widget to display video output.
-        child: Video(controller: controller),
-      ),
-    );
+    return Scaffold(
+        body: Column(
+      children: [
+        const Padding(
+          padding: EdgeInsets.all(32.0),
+          child: Image(image: AssetImage("images/ganatiempo.png")),
+        ),
+        SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.width * 9.0 / 16.0,
+          // Use [Video] widget to display video output.
+          child: Video(controller: controller),
+        ),
+      ],
+    ));
   }
 }
